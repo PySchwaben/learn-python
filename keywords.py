1d2bf8ffc61da0bca4bcac110138b6e5409e9e34
Oliver Bestwalter <oliver@bestwalter.de>
2014-08-31 20:30:53 +0200

    basics

---
+++ keywords.py
@@ -0,0 +1,45 @@
+
+True, False
+is
+
+None
+
+and, not, or
+
+break
+continue
+pass
+
+def
+return
+
+class
+
+del
+
+import
+from
+
+if
+elif
+else
+
+for
+in
+while
+
+try
+except
+finally
+raise
+
+with
+as
+
+
+lambda
+yield
+
+global, nonlocal
+
+assert
